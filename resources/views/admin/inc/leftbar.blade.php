34403dad96aba3e4330b0536fb498b236008d6ee
brain <shahriarmehedi95@gmail.com>
2021-09-23 01:43:18 +0600

    dashboard commit

---
+++ resources/views/admin/inc/leftbar.blade.php
@@ -0,0 +1,42 @@
+
+    <!-- ########## START: LEFT PANEL ########## -->
+    <div class="sl-logo"><a href=""><i class="icon ion-android-star-outline"></i> starlight</a></div>
+    <div class="sl-sideleft">
+ 
+
+      <div class="sl-sideleft-menu">
+        <a href="{{url('/')}}" class="sl-menu-link " target="_blank">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+            <span class="menu-item-label">Visit Site</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+
+        <div class="sl-sideleft-menu">
+        <a href="index.html" class="sl-menu-link active">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+            <span class="menu-item-label">Dashboard</span>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+       
+        <a href="#" class="sl-menu-link">
+          <div class="sl-menu-item">
+            <i class="menu-item-icon ion-ios-pie-outline tx-20"></i>
+            <span class="menu-item-label">Charts</span>
+            <i class="menu-item-arrow fa fa-angle-down"></i>
+          </div><!-- menu-item -->
+        </a><!-- sl-menu-link -->
+        <ul class="sl-menu-sub nav flex-column">
+          <li class="nav-item"><a href="chart-morris.html" class="nav-link">Morris Charts</a></li>
+          <li class="nav-item"><a href="chart-flot.html" class="nav-link">Flot Charts</a></li>
+          <li class="nav-item"><a href="chart-chartjs.html" class="nav-link">Chart JS</a></li>
+          <li class="nav-item"><a href="chart-rickshaw.html" class="nav-link">Rickshaw</a></li>
+          <li class="nav-item"><a href="chart-sparkline.html" class="nav-link">Sparkline</a></li>
+        </ul>
+       
+      </div><!-- sl-sideleft-menu -->
+
+      <br>
+    </div><!-- sl-sideleft -->
+    <!-- ########## END: LEFT PANEL ########## -->
