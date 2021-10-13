dcd11a64973a05815da044df1848dc7c34f97122
brain <shahriarmehedi95@gmail.com>
2021-10-13 23:50:58 +0600

    payment done

--- resources/views/frontend/payment/hostedPayment.blade.php
+++ resources/views/frontend/payment/hostedPayment.blade.php
@@ -12,6 +12,7 @@
 	</div><!-- /.container -->
 </div><!-- /.breadcrumb -->
 
+
 <div class="body-content">
 	<div class="container">
 		<div class="checkout-box ">
@@ -23,41 +24,37 @@
                     </h4>
                     <ul class="list-group mb-3">
                         @foreach ($carts as $item)
-                            
-                     
                         <li class="list-group-item d-flex justify-content-between lh-condensed">
                             <div>
                                 <h6 class="my-0">{{ $item->name }}</h6>
-                                <span>Color : <small class="text-muted">{{ $item->options->color }}</small></span> 
-                                <br>
-                               <span>Size : <small class="text-muted">{{ $item->options->size }}</small></span> 
+                                <small class="text-muted">{{ $item->options->color }}</small> <br>
+                                <small class="text-muted">{{ $item->options->size }}</small>
                             </div>
-                            <span>Price : <small class="text-muted">{{ $item->price }}</small></span> 
+                            <span class="text-muted">{{ $item->price }}tk</span>
                         </li>
                         @endforeach
-             
-            
+
+
                         <li class="list-group-item d-flex justify-content-between">
                             <span>Total (BDT)</span>
-                            <strong>{{ $total_amount }}</strong>
+                            <strong>{{ $total_amount }} TK</strong>
                         </li>
                     </ul>
                 </div>
                 <div class="col-md-8 order-md-1">
-                  
-                    <form action="{{ url('/pay') }}" method="POST" class="needs-validation" novalidate>
-                        
-                    <input type="hidden" value="{{ csrf_token() }}" name="_token" />
-                    <input type="hidden" value="{{ $total_amount }}" name="amount" id="total_amount" required/>
-                    <input type="hidden" name="name" value="{{ $data['shipping_name'] }}">
-                    <input type="hidden" name="email" value="{{ $data['shipping_email'] }}">
-                    <input type="hidden" name="phone" value="{{ $data['shipping_phone'] }}">
-                    <input type="hidden" name="post_code" value="{{ $data['post_code'] }}">
-                    <input type="hidden" name="division_id" value="{{ $data['division_id'] }}">
-                    <input type="hidden" name="district_id" value="{{ $data['district_id'] }}">
-                    <input type="hidden" name="state_id" value="{{ $data['state_id'] }}">
-                    <input type="hidden" name="notes" value="{{ $data['notes'] }}">
-                
+                    <form action="{{ url('/pay') }}" method="POST" class="needs-validation">
+
+                        <input type="hidden" value="{{ csrf_token() }}" name="_token" />
+                        <input type="hidden" value="{{ $total_amount }}" name="amount" id="total_amount" required/>
+                        <input type="hidden" name="name" value="{{ $data['shipping_name'] }}">
+                        <input type="hidden" name="email" value="{{ $data['shipping_email'] }}">
+                        <input type="hidden" name="phone" value="{{ $data['shipping_phone'] }}">
+                        <input type="hidden" name="post_code" value="{{ $data['post_code'] }}">
+                        <input type="hidden" name="division_id" value="{{ $data['division_id'] }}">
+                        <input type="hidden" name="district_id" value="{{ $data['district_id'] }}">
+                        <input type="hidden" name="state_id" value="{{ $data['state_id'] }}">
+                        <input type="hidden" name="notes" value="{{ $data['notes'] }}">
+
                         <hr class="mb-4">
                         <div class="custom-control custom-checkbox">
                             <input type="checkbox" class="custom-control-input" id="same-address">
@@ -69,16 +66,13 @@
                             <label class="custom-control-label" for="save-info">Save this information for next time</label>
                         </div>
                         <hr class="mb-4">
-                        <button class="btn btn-primary btn-lg btn-block" id="sslczPayBtn"
-                                token="if you have any token validation"
-                                postdata="your javascript arrays or objects which requires in backend"
-                                order="If you already have the transaction generated for current order"
-                                endpoint="{{ url('/pay-via-ajax') }}"> Pay Now
-                        </button>
+                        <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
+                        
                     </form>
                 </div>
             </div>
-        </div>
-    </div>
+		</div>
+	</div>
 </div>
+
 @endsection
